51002a836fbb69e37f56529bf0ef3435373b6d21
Matthew Radcliffe <mradcliffe@student.neumont.edu>
2015-07-07 15:52:32 -0600

    Committing test Directory and playing around with code in index

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 #! /usr/bin/env node
 console.log('it worked')
+
+var program = require('commander');
+
+program
+	.version('0.0.1')
+	.option('-M , --major', 'Bump major')
+	.option('-m , --minor', 'Bump minor')
+	.option('-p , --patch', 'Bump patch')
+	.option('-b , --build', 'Building src into lib')
+	.option('-t , --tag', 'Git tag the repo')
+	.option('-F , --fullbuild', 'Do all the things')
+	.parse(process.argv);
+	
+if (program.major) console.log("Bumping major");
+if (program.minor) console.log("Bumping minor");
+if (program.patch) console.log("Bumping patch");
+if (program.build) console.log("Build command executed");
+if (program.tag) console.log("Gittag called");
+if (program.fullbuild) console.log("fullbuild called");
---
+++ test/all.js
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+
+
+exports['Test'] = function(assert, done)
+{
+	
+}
