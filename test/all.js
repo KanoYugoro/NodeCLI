686b2216a279fc0d5db7f1874bc5e3f34ebcd876
Anson Wayman <kanoyugoro@gmail.com>
2015-07-08 15:59:23 -0600

    Added mocha tests and played around a bit

--- test/all.js
+++ test/all.js
@@ -1,18 +1,31 @@
 #! /usr/bin/env node
 
-var assert = require('assert')
+var versionbumper = require('../bin/versionbumper.js');
+var expect = require('chai').expect;
 
-function testVersionBumpPatch()
-{
-	var fs = require('fs');
-	var fsFile = fs.readFileSync("./package.json");
-	var fsString = fsFile.toString();
-	var result = JSON.parse(fsString);
-	
-	console.log(result.version);
-	assert.equal(2+2,4,"truthful");
-}
-
-if (module == require.main) testVersionBumpPatch()
-
-
+describe("versionbumper", function(){
+   describe("#bumpPatch()", function(){
+       it("should bump up the third value in the string passed in.", function(){
+           var arg = "1.0.0";
+           var result = versionbumper.bumpPatch(arg);
+ 
+           expect(result).to.equal("1.0.1");
+       });
+   });
+   describe("#bumpMinor()", function(){
+       it("should bump up the second value in the string passed in.", function(){
+           var arg = "1.0.0";
+           var result = versionbumper.bumpMinor(arg);
+ 
+           expect(result).to.equal("1.1.0");
+       });
+   });
+   describe("#bumpMajor()", function(){
+       it("should bump up the first value in the string passed in.", function(){
+           var arg = "1.0.0";
+           var result = versionbumper.bumpMajor(arg);
+ 
+           expect(result).to.equal("2.0.0");
+       });
+   });
+});
