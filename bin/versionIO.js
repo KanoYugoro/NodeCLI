efd0ef1c5ef2507d304f788ab4e2591da6038e25
Anson Wayman <kanoyugoro@gmail.com>
2015-07-13 16:27:28 -0600

    Added code to log the error and changed the name of the second error variable to avoid confusion #12.

--- bin/versionIO.js
+++ bin/versionIO.js
@@ -1,20 +1,22 @@
 exports = module.exports = {}
 var fs = require('fs');
 
-
-
 exports.writeVersion = function(file,newversion,callback)
 {
 	fs.readFile(file, function (err, data) {			
+		if (err)
+		{
+			console.log(err)
+		}
 		var toparse = data.toString();
 		var result = JSON.parse(toparse);
 		var oldversion = result.version;
 		result.version = newversion;
 		var towrite = JSON.stringify(result, null, 4);
-		fs.writeFile(file,towrite,function(err){
-			if (err)
+		fs.writeFile(file,towrite,function(otherErr){
+			if (otherErr)
 			{
-				console.log(err);
+				console.log(otherErr);
 			}
 			callback(oldversion,result.version);
 		});
