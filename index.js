1249eac0f1ce9d181693fbaed0e1a418d93a1fae
Anson Wayman <kanoyugoro@gmail.com>
2015-07-15 16:25:08 -0600

    tabbed the file properly #5

--- index.js
+++ index.js
@@ -5,63 +5,64 @@ var gitfunctions = require('./bin/gitfunctions.js');
 var versionIO = require('./bin/versionIO.js');
 var versionbumper = require('./bin/versionbumper.js');
 
-versionIO.readVersion('package.json',function(version) {
+versionIO.readVersion('package.json',function(version) 
+{
 	var packageversion = version;
 
-program
-	.version('0.0.1')
-	.option('-a , --alpha', 'Bump alpha')
-	.option('-b , --beta',  'Bump beta')
-	.option('-M , --major', 'Bump major')
-	.option('-m , --minor', 'Bump minor')
-	.option('-p , --patch', 'Bump patch')
-	.option('-t , --tag [message]', 'Git tag the repo')
-	.option('-c , --commit [message]', 'Commit to git')
-	.option('-P , --push', 'Push to the repo')
-	.parse(process.argv);
-	
-if (program.major)
-{
-	packageversion = versionbumper.bumpMajor(packageversion);
-}
-else
-if (program.minor)
-{
-	packageversion = versionbumper.bumpMinor(packageversion);
-}
-else
-if (program.patch)
-{
-	packageversion = versionbumper.bumpPatch(packageversion);
-}
-else
-if (program.beta)
-{
-	packageversion = versionbumper.bumpBeta(packageversion);
-}
-else
-if (program.alpha)
-{
-	packageversion = versionbumper.bumpAlpha(packageversion);
-}
+	program
+		.version('0.0.1')
+		.option('-a , --alpha', 'Bump alpha')
+		.option('-b , --beta',  'Bump beta')
+		.option('-M , --major', 'Bump major')
+		.option('-m , --minor', 'Bump minor')
+		.option('-p , --patch', 'Bump patch')
+		.option('-t , --tag [message]', 'Git tag the repo')
+		.option('-c , --commit [message]', 'Commit to git')
+		.option('-P , --push', 'Push to the repo')
+		.parse(process.argv);
+		
+	if (program.major)
+	{
+		packageversion = versionbumper.bumpMajor(packageversion);
+	}
+	else
+	if (program.minor)
+	{
+		packageversion = versionbumper.bumpMinor(packageversion);
+	}
+	else
+	if (program.patch)
+	{
+		packageversion = versionbumper.bumpPatch(packageversion);
+	}
+	else
+	if (program.beta)
+	{
+		packageversion = versionbumper.bumpBeta(packageversion);
+	}
+	else
+	if (program.alpha)
+	{
+		packageversion = versionbumper.bumpAlpha(packageversion);
+	}
 
-if (program.major || program.minor || program.patch)
-{
-	versionIO.writeVersion('package.json',packageversion,function(oldversion,newversion){});
-}
+	if (program.major || program.minor || program.patch)
+	{
+		versionIO.writeVersion('package.json',packageversion,function(oldversion,newversion){});
+	}
 
-if (program.tag)
-{
-	gitfunctions.gitTag(packageversion,program.tag);
-}
+	if (program.tag)
+	{
+		gitfunctions.gitTag(packageversion,program.tag);
+	}
 
-if (program.commit) 
-{
-	gitfunctions.gitCommit(program.commit);
-}
+	if (program.commit) 
+	{
+		gitfunctions.gitCommit(program.commit);
+	}
 
-if (program.push)
-{
-	gitfunctions.gitPush();
-}
+	if (program.push)
+	{
+		gitfunctions.gitPush();
+	}
 });
